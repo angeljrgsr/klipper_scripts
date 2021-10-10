04b25e0444536a39429c1630373792f1bd41bf37
Angel R. Rojas, Jr <91292560+angeljrgsr@users.noreply.github.com>
2021-10-10 08:53:26 -0400

    Update backup_klipper.sh

--- backup_klipper.sh
+++ backup_klipper.sh
@@ -1,5 +1,6 @@
 cp ~/.moonraker_database/*.mdb ~/klipper_config/Backup_Moonraker/.
 cd ~/klipper_config/
+rm -r printer-2*.cfg
 TIMESTAMP=`date +%y-%m-%d_%T`
 git add .
 git commit -m "Backup_$TIMESTAMP"
